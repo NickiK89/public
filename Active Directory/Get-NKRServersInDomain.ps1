dac5ae5dbf835cdb9c3e068e2c712ab2ebdabe04
Nicki Kristensen <nkr@nobia.dk>
2020-07-15 10:37:27 +0200

    Added comment

--- Active Directory/Get-NKRServersInDomain.ps1
+++ Active Directory/Get-NKRServersInDomain.ps1
@@ -4,3 +4,5 @@ Import-Module ActiveDirectory
 # Get all servers
 $Servers = Get-ADComputer -Filter 'OperatingSystem -like "*server*" -and Enabled -eq "true"' -Properties Name,OperatingSystem,OperatingSystemVersion,IPv4Address
 $Servers | Sort-Object -Property IPv4Address | Select-Object -Property Name, OperatingSystem, OperatingSystemVersion, IPv4Address
+
+# Comment
