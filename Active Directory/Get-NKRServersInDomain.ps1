fc0300c5f084c6496b1c0d02e00a3bcfb28566c5
Nicki Kristensen <nkr@nobia.dk>
2020-07-15 10:34:08 +0200

    First version

---
+++ Active Directory/Get-NKRServersInDomain.ps1
@@ -0,0 +1,6 @@
+# Import the RSAT AD module
+Import-Module ActiveDirectory
+
+# Get all servers
+$Servers = Get-ADComputer -Filter 'OperatingSystem -like "*server*" -and Enabled -eq "true"' -Properties Name,OperatingSystem,OperatingSystemVersion,IPv4Address
+$Servers | Sort-Object -Property IPv4Address | Select-Object -Property Name, OperatingSystem, OperatingSystemVersion, IPv4Address
