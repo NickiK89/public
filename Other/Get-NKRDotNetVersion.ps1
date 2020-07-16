d2161d896ec7265efffc37a0aa4616dbdc5a017b
Nicki Kristensen <nkr@nobia.dk>
2020-07-16 09:17:51 +0200

    Created Get-NKRDotNetVersion

---
+++ Other/Get-NKRDotNetVersion.ps1
@@ -0,0 +1,12 @@
+function Get-NKRDotNetVersion
+{
+    Write-Host 'Installed .NET Framework Components' -ForegroundColor Yellow
+    Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP' -Recurse |
+    Get-ItemProperty -Name Version,Release -ErrorAction 0 |
+    Where-Object { $_.PSChildName -match '^(?!S)\p{L}'} |
+    Where-Object {$_.PSChildName -match "Client" -or "Full"} |
+    Select-Object -Property @{N='.NET Component';E={$_.PSChildName}}, @{N='Version';E={$_.Version}} |
+    Sort-Object -Property Version -Descending |
+    Format-Table -AutoSize
+}
+Get-NKRDotNetVersion
