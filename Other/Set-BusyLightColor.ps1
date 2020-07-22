97cdd1f4edc65ef1720e37a1041c9177df03bb92
Nicki Kristensen <nkr@nobia.dk>
2020-07-22 14:24:32 +0200

    Testing increment function

--- Other/Set-BusyLightColor.ps1
+++ Other/Set-BusyLightColor.ps1
@@ -1,13 +1,9 @@
 # Sideproject to change color on Kuando Busylight
-# As all gamers know, RGB = FPS ;-)
+# As all gamers know, more RGB = more FPS ;-)
 
 #
 [CmdletBinding()]
 param(
-    # Mandatory parameters
-
-    [Parameter(Mandatory)]
-    [string]$FilePath
 )
 
 function New-Stuff {
@@ -18,3 +14,23 @@ function New-Stuff {
 
     # Enter code to do something here
 }
+
+# Get random number between 1 and 255
+$Number = 250 #Get-Random -Minimum 1 -Maximum 255
+
+# Increment random number
+
+switch ($x) {
+    condition {  }
+    Default {}
+} ($Number -le 255) {
+    $Number++
+    Write-Host $Number
+    Start-Sleep -Milliseconds 250
+}
+
+for ($Number = $Number; $Number -le 255; $Number++) {
+        
+    } 
+}
+    
