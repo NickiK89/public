24a3568189607fbdbe76e63ea861181fc301c734
Nicki Kristensen <nkr@nobia.dk>
2020-07-22 12:04:34 +0200

    First commit

---
+++ Other/Set-BusyLightColor.ps1
@@ -0,0 +1,20 @@
+# Sideproject to change color on Kuando Busylight
+# As all gamers know, RGB = FPS ;-)
+
+#
+[CmdletBinding()]
+param(
+    # Mandatory parameters
+
+    [Parameter(Mandatory)]
+    [string]$FilePath
+)
+
+function New-Stuff {
+    param(
+        [Parameter()]
+        [string]$FilePath
+    )
+
+    # Enter code to do something here
+}
