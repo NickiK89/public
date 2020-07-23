1bd049b2b627b08a45a9633f7fe8f7a6d569cc3d
Nicki Kristensen <nkr@nobia.dk>
2020-07-23 09:08:24 +0200

    Checks for record

--- Other/Set-DnsRecords.ps1
+++ Other/Set-DnsRecords.ps1
@@ -1,4 +1,5 @@
-# Used to 
+# Used to update the DNS server with records of peripherals
+# Imports a CSV with IP addresses and the hostname of the device(s)
 
 #requires -Module DnsServer
 [CmdletBinding()]
@@ -6,14 +7,24 @@ param(
     # Mandatory parameters
 
     [Parameter(Mandatory)]
-    [string]$FilePath
+    [string]$FilePath,
+    [string]$ServerName
 )
+
+# Get domainname
+$DomainName = $env:USERDNSDOMAIN
 
 function Set-DnsRecords {
     param(
         [Parameter()]
         [string]$FilePath
     )
-    $Subnet = Import-Csv C:\Users\nkr\Desktop\Subnet254.csv
-    $Subnet
+    
+    $Records = Import-Csv $FilePath
+
+    # Check if record already exists
+    foreach ($record in $Records) {
+        Resolve-DnsName -Name $record -Type A -Server $ServerName -DnsOnly
+    }    
+    Add-DnsServerResourceRecordA
 }
