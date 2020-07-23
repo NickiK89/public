0f792a1b42a19d75a101d3e04422f29b56a558d2
Nicki Kristensen <nkr@nobia.dk>
2020-07-23 11:08:50 +0200

    Re-written script

--- Active Directory/Get-NKRServersInDomain.ps1
+++ Active Directory/Get-NKRServersInDomain.ps1
@@ -1,6 +1,13 @@
-# Import the RSAT AD module
-Import-Module ActiveDirectory
+# Retrieves all active servers in the current userdomain
 
-# Get all servers
-$Servers = Get-ADComputer -Filter 'OperatingSystem -like "*server*" -and Enabled -eq "true"' -Properties Name,OperatingSystem,OperatingSystemVersion,IPv4Address
-$Servers | Sort-Object -Property Name | Select-Object -Property Name, OperatingSystem, OperatingSystemVersion, IPv4Address | Out-GridView
+#requires -Module ActiveDirectory
+[CmdletBinding()]
+param(
+)
+
+function Get-ServersInAd {
+    # Get all servers
+    $Servers = Get-ADComputer -Filter 'OperatingSystem -like "*server*" -and Enabled -eq "true"' -Properties Name,OperatingSystem,OperatingSystemVersion,IPv4Address
+    $Servers | Sort-Object -Property Name | Select-Object -Property Name, OperatingSystem, OperatingSystemVersion, IPv4Address | Out-GridView
+}
+Get-ServersInAd
